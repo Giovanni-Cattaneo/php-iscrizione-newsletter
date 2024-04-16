d7b1f38aef3139a8653d8071ebe4de0db9d004b2
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-16 14:16:02 +0200

    add header

--- index.php
+++ index.php
@@ -11,11 +11,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: antiquewhite;
+        }
+
+        header {
+            background-color: turquoise;
+        }
+    </style>
 
 </head>
 
 <body>
-    <header></header>
+    <header class="d-flex align-items-center justify-content-between p-3">
+        <h1>Welcome to Bolean News</h1>
+        <div class="search">
+            <input type="text">
+            <button>Cerca</button>
+        </div>
+
+    </header>
 
     <main></main>
 
